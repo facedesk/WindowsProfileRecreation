46862c2968fd2f8072cf434a12cc0be4f0f75906
facedesk <dgold@sjnma.org>
2016-10-17 12:46:23 -0500

    Update script to post virtual machine testing

--- Recreate.bat
+++ Recreate.bat
@@ -1,4 +1,4 @@
-set profile=LabUser
+set profile=labuser
 
 	rem log out all users except that running this script
 @echo off
@@ -6,10 +6,14 @@ query session > session.txt
 for /f "skip=2 tokens=2,3,4" %%i in (session.txt) DO if not "%%i"== "%USERNAME%"  logoff %%j
 	
 	rem delete local profile
+echo  deleting C:\Users\%profile%
 RMDIR /S /Q C:\Users\%profile%
-rem net user LabUser13 /del
+RMDIR /S /Q C:\Users\TEMP
 
-rem net user LabUser13 LabUser13 /add
+
+net user %profile% /del
+net user %profile% %profile% /add
+
 
 
 del session.txt
@@ -17,7 +21,3 @@ del session.txt
 
 	rem log user in- this would be nice to have
 	rem shut down
-
-
-
-
